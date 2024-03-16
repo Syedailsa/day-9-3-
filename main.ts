26f5ee046b33578bdc51acc9a627e4615a8c6444
Ilsa. <ilsasyeda@gmail.com>
2024-03-16 15:20:49 +0500

    first commit

---
+++ main.ts
@@ -0,0 +1,11 @@
+var alein_color: string = "red";
+
+if(alein_color == "Green"){
+    console.log('alien is alive')
+}else if(alein_color == "yellow"){
+    console.log('alien`s life is 30% left')
+}else if(alein_color == "red"){
+    console.log('alien is dead you earned 50 points!')
+}else{
+    console.log('GAME OVER')
+};
